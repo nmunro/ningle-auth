965fb1bb54ac5b7b769640c6b4b3fd58ca6ce8b0
NMunro <nmunro@duck.com>
2025-06-15 21:50:02 +0100

    Added delete route

--- src/main.lisp
+++ src/main.lisp
@@ -11,25 +11,29 @@
 (djula:add-template-directory (asdf:system-relative-pathname :ningle-auth "src/templates/"))
 
 (setf (ningle:route *app* "/register")
-      (lambda (params)
+    (lambda (params)
         (format t "Test: ~A~%" (mito:retrieve-by-sql "SELECT 2 + 3 AS result"))
         (djula:render-template* "auth/register.html" nil :title "Register")))
 
 (setf (ningle:route *app* "/login")
-      (lambda (params)
+    (lambda (params)
         (djula:render-template* "auth/login.html" nil :title "Login")))
 
 (setf (ningle:route *app* "/logout")
-      (lambda (params)
+    (lambda (params)
         (djula:render-template* "auth/logout.html" nil :title "Logout")))
 
 (setf (ningle:route *app* "/reset")
-      (lambda (params)
+    (lambda (params)
         (djula:render-template* "auth/reset.html" nil :title "Reset")))
 
 (setf (ningle:route *app* "/verify")
-      (lambda (params)
+    (lambda (params)
         (djula:render-template* "auth/verify.html" nil :title "Verify")))
+
+(setf (ningle:route *app* "/delete")
+    (lambda (params)
+        (djula:render-template* "auth/delete.html" nil :title "Delete")))
 
 (defmethod ningle:not-found ((app ningle:<app>))
     (declare (ignore app))
